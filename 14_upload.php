aea165c32254c80d8d337409fc64e181ef4f99bc
Isabelli <isabelli.pinto@aluno.senai.br>
2025-10-24 16:31:43 -0300

    realização dos exercicios.

--- 14_upload.php
+++ 14_upload.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Upload de Imagem</title>
+</head>
+<body>
+    <label for="imagem">Selecione uma imagem:</label>
+    <form method="post" action="" enctype="ultipart/form-data">
+    </form>
+</body>
+</html>
