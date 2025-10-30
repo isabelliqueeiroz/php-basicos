aaa27de76bbeb212219123ee56745b1514feb911
Isabelli <isabelli.pinto@aluno.senai.br>
2025-10-30 11:12:14 -0300

    até o exemplo 14

--- 14_upload.php
+++ 14_upload.php
@@ -2,11 +2,39 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Upload de Imagem</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload de imagem</title>
 </head>
 <body>
-    <label for="imagem">Selecione uma imagem:</label>
-    <form method="post" action="" enctype="ultipart/form-data">
-    </form>
+    <form method="post" action="" enctype="multipart/form-data">
+        <label for="imagem">Selecione uma imagem:</label>
+        <input type="file" name="imagem" accpt="imagem/*" required><br><br>
+
+        <button type="submit">Upload</button>
+</form>
+
+<?php
+// Verifica se o formulário foi enviado
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $diretorio_destino = 'uploads/';
+
+    // Verifica se a pasta existe, caso não, cria a pasta
+    if (!is_dir($diretorio_destino)) {
+        mkdir($diretorio_destino, 0777, true);
+    }
+
+    $nome_arquivo = basename($_FILES['imagem']['name']);
+    $caminho_completo = $diretorio_destino . $nome_arquivo;
+
+    // Move o arquivo enviado para o diretório de destino
+    if(move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho_completo)) {
+        echo "<p>Upload realizado com sucesso!</p>";
+        echo "<img src='$caminho_completo' alt='Imagem enviada' style='max-width: 300px;'>";
+    } else {
+        echo "<p>Erro ao fazer upload do arquivo.</p>";
+    }
+}
+?>  
 </body>
 </html>
+
